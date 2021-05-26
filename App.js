1e6041942f2012aac2eae7bbdc9021e60adee11b
cluster05 <ajayakumbhar1234@gmail.com>
2021-05-26 21:36:04 +0530

    tab navigation icons added.

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+import { Feather } from '@expo/vector-icons';
+
 import HomeScreen from './src/screen/Home';
 import FilterScreen from './src/screen/Filter';
 import MeScreen from './src/screen/Me';
@@ -14,10 +16,51 @@ export default function App() {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name="Home" component={HomeScreen} />
-          <Tab.Screen name="Filter" component={FilterScreen} />
-          <Tab.Screen name="Screen" component={MeScreen} />
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+              let iconSize = 24;
+              let iconColor = 'gray';
+
+              const setActiveIcon = () => {
+                iconSize = focused ? 28 : 24;
+                iconColor = focused ? 'black' : 'gray';
+              }
+
+              if (route.name === 'Home') {
+                iconName = "home";
+                setActiveIcon();
+              } else if (route.name === 'Filter') {
+                iconName = "search"
+                setActiveIcon();
+              } else if (route.name === 'Me') {
+                iconName = "user"
+                setActiveIcon();
+              } else {
+                iconName = "home"
+              }
+
+              return <Feather name={iconName} size={iconSize} color={iconColor} />;
+            },
+          })}
+
+          tabBarOptions={{
+            showLabel: false,
+          }}
+        >
+          <Tab.Screen
+            name="Home"
+            component={HomeScreen}
+          />
+          <Tab.Screen
+            name="Filter"
+            component={FilterScreen}
+          />
+          <Tab.Screen
+            name="Me"
+            component={MeScreen}
+          />
         </Tab.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
